13803ddd6310af22f3dcc48676b38498691eb9fd
DESKTOP-QITT7RJ\q <ghqls1237@kaist.ac.kr>
2020-12-29 20:26:56 +0900

    초기새팅 - tab

---
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager vp = findViewById(R.id.viewpager);
+        VPAdapter adapter = new VPAdapter(getSupportFragmentManager());
+        vp.setAdapter(adapter);
+
+        //tab과 viewpager 연동시키는 과정
+        TabLayout tab=findViewById(R.id.tab);
+        tab.setupWithViewPager(vp);
+
+
+
+    }
+}
