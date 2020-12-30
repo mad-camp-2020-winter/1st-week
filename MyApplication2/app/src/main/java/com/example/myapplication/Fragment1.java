d28daec11f577e885b4b89a7ce77c80164326a54
DESKTOP-QITT7RJ\q <ghqls1237@kaist.ac.kr>
2020-12-30 21:20:52 +0900

    전화번호부 - 전화걸기 구현 전

--- MyApplication2/app/src/main/java/com/example/myapplication/Fragment1.java
+++ MyApplication2/app/src/main/java/com/example/myapplication/Fragment1.java
@@ -1,8 +1,15 @@
 package com.example.myapplication;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,14 +18,20 @@ import android.view.ViewGroup;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import android.graphics.drawable.Drawable;
+import android.widget.Toast;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -31,11 +44,14 @@ public class Fragment1 extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    private static final String[] NAME ={"0","0","0","0","0","0","0","0","0","0"};
-    private static final String[] PHONE = {"0","0","0","0","0","0","0","0","0","0"};
+    private static String[] NAME;
+    private static String[] PHONE;
+    private static String[] FINAL_LIST;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private int len;
+    private final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 1000;
 
 
     //json 파일을 스트링으로 읽어오기
@@ -68,9 +84,12 @@ public class Fragment1 extends Fragment {
 
         String str = getJasonString();
 
+
         try {
             JSONArray jarray = new JSONArray(str);
-            int len = jarray.length();
+            len = jarray.length();
+            NAME = new String[len];
+            PHONE = new String[len];
             for (int i=0;i<len;i++){
                 JSONObject jObject = jarray.getJSONObject(i);
                 String name = jObject.getString("name");
@@ -115,7 +134,7 @@ public class Fragment1 extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-        doJSONParser();
+
     }
 
     public int findNum(String text){
@@ -124,35 +143,114 @@ public class Fragment1 extends Fragment {
                 return i;
             };
         };
-        return 0;
+        return -1;
     }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permission[], int[] grantResults){
+        switch (requestCode){
+            case MY_PERMISSIONS_REQUEST_CALL_PHONE:
+            {
+                if (grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(getActivity(),"승인이 허가되어 있습니다", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(getActivity(),"아직 승인받지 않았습니다.", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_1, null);
+        ListViewAdapter adapter;
 
-        ArrayAdapter adapter = new ArrayAdapter(getActivity(),android.R.layout.simple_list_item_1, NAME);
+
+        doJSONParser();
+        FINAL_LIST = new String[len];
+
+        for (int i =0; i<len; i++){
+            FINAL_LIST[i] = NAME[i];
+        }
+        String[] original = FINAL_LIST;
+
+        //ArrayAdapter adapter = new ArrayAdapter(getActivity(),android.R.layout.simple_list_item_1, FINAL_LIST);
+        adapter = new ListViewAdapter() ;
+
         //LIST_MENU에 json으로부터 읽은 파일 넣기
 
         ListView listview = (ListView) view.findViewById(R.id.listview1);
         listview.setAdapter(adapter);
 
+        for (int i=0; i< len; i++){
+            adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.human),FINAL_LIST[i]);
+        }
+
+
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String strText = (String) parent.getItemAtPosition(position);
 
-                int index = findNum(strText);
+                ListViewItem item = (ListViewItem) parent.getItemAtPosition(position) ;
 
+                String titleStr = item.getTitle() ;
+                Drawable iconDrawable = item.getIcon() ;
 
-                NAME[index] = PHONE[index];
+                // TODO : use item data.
 
-                System.out.println(NAME[index]);
+                ListViewAdapter adapter;
+                adapter = new ListViewAdapter() ;
                 listview.setAdapter(adapter);
 
-            };
-        });
+                FINAL_LIST = original;
+                int index = findNum(titleStr);
+
+                if (index>=0) {
+                    FINAL_LIST = new String[len+1];
+                    for (int i = 0; i < len + 1; i++) {
+                        if (i <= index) {
+                            FINAL_LIST[i] = NAME[i];
+                        } else if (i == index + 1) {
+                            FINAL_LIST[i] = PHONE[index];
+
+                        } else {
+                            FINAL_LIST[i] = NAME[i - 1];
+                        }
+                    }
+                    for (int i = 0; i<FINAL_LIST.length; i++) {
+                        if (i == index+1 && FINAL_LIST.length > len) {
+                            adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.phone), FINAL_LIST[i]);
+                        }
+                        else{
+                            adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.human), FINAL_LIST[i]);
+                        }
+                    }
+                }
+                else{
+
+                    int permissionCheck = ContextCompat.checkSelfPermission(getActivity(),Manifest.permission.CALL_PHONE);
+
+                    if (permissionCheck != PackageManager.PERMISSION_GRANTED){
+                        Toast.makeText(getActivity(),"권한 승인이 필요합니다.",Toast.LENGTH_LONG).show();
+
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),Manifest.permission.CALL_PHONE)){
+                            Toast.makeText(getActivity(),"전화 권한 필요함", Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+
+                    String tel = "tel:" + titleStr;
+                    startActivity(new Intent("android.intent.action.CALL", Uri.parse(tel)));
+                    //startActivity(new Intent("android.intent.action.DIAL", Uri.parse(tel)));
+
+                }
+            }
+        }) ;
 
         return view;
     }
