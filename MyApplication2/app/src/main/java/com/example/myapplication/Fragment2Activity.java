aab1c3afd486d57da2e6a71115e3a476f94c4279
zeroStone <lysuk96@hanyang.ac.kr>
2020-12-31 16:59:52 +0900

    갤러리 디스플레이 맞춤형 크기 조절

--- MyApplication2/app/src/main/java/com/example/myapplication/Fragment2Activity.java
+++ MyApplication2/app/src/main/java/com/example/myapplication/Fragment2Activity.java
@@ -20,14 +20,14 @@ public class Fragment2Activity extends Activity implements View.OnClickListener 
 
 
     //추가 변수
-    final int IMAGEMAX = 9;
+    final int IMAGEMAX = 21;
     MainActivity activity;
     int position;
 
-    View t1, t2, t3, t4, t5, t6, t7, t8, t9;
-    private Integer[] images = {R.drawable.a1, R.drawable.a2, R.drawable.a3, R.drawable.a4, R.drawable.a5, R.drawable.a6, R.drawable.a7, R.drawable.a8, R.drawable.a9};
-    private Integer[] thumbButton = {R.id.thumb_button_1, R.id.thumb_button_2, R.id.thumb_button_3, R.id.thumb_button_4, R.id.thumb_button_5, R.id.thumb_button_6, R.id.thumb_button_7, R.id.thumb_button_8, R.id.thumb_button_9};
-    private View[] thumbView = {t1, t2, t3, t4, t5, t6, t7, t8, t9};
+    View t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20, t21;
+    private Integer[] images = {R.drawable.a1, R.drawable.a2, R.drawable.a3, R.drawable.a4, R.drawable.a5, R.drawable.a6, R.drawable.a7, R.drawable.a8, R.drawable.a9, R.drawable.a10, R.drawable.a11, R.drawable.a12, R.drawable.a13, R.drawable.a14, R.drawable.a15, R.drawable.a16, R.drawable.a17, R.drawable.a18, R.drawable.a19, R.drawable.a20, R.drawable.a21};
+    private Integer[] thumbButton = {R.id.thumb_button_1, R.id.thumb_button_2, R.id.thumb_button_3, R.id.thumb_button_4, R.id.thumb_button_5, R.id.thumb_button_6, R.id.thumb_button_7, R.id.thumb_button_8, R.id.thumb_button_9, R.id.thumb_button_10, R.id.thumb_button_11, R.id.thumb_button_12, R.id.thumb_button_13, R.id.thumb_button_14, R.id.thumb_button_15, R.id.thumb_button_16, R.id.thumb_button_17, R.id.thumb_button_18, R.id.thumb_button_19, R.id.thumb_button_20, R.id.thumb_button_21};
+    private View[] thumbView = {t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20, t21};
 
     public Fragment2Activity(){
 
