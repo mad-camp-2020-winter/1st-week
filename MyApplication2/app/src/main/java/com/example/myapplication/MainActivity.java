55fdd70c735954a7812c974fabc1a434cb52efb7
zeroStone <lysuk96@hanyang.ac.kr>
2020-12-31 12:36:25 +0900

    fragment간 데이터 전달 코드

--- MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -37,16 +37,12 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    Fragment2 fragment2 = new Fragment2();
-    Fragment2Detail fragment2Detail = new Fragment2Detail();
+    private Integer[] images = {R.drawable.a1, R.drawable.a2, R.drawable.a3, R.drawable.a4, R.drawable.a5, R.drawable.a6, R.drawable.a7, R.drawable.a8, R.drawable.a9};
 
     public void onFragmentChange(int index) {
-        if(index == 0) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment2).commit();
-        }
-        else if (index ==1) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment2Detail).commit();
-        }
+        Fragment2Detail fragment2Detail = new Fragment2Detail(index);
+//        fragment2Detail.zoomImageFromThumb(R.drawable.a1);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment2Detail).commit();
     }
 
 
