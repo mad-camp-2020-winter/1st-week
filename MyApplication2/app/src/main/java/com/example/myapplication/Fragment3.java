0fad1d663e25607b641cfbb70e4d562207c0e6da
DESKTOP-QITT7RJ\q <ghqls1237@kaist.ac.kr>
2021-01-03 23:10:11 +0900

    이상형월드컵 - 기본 틀만 잡아놓음

--- MyApplication2/app/src/main/java/com/example/myapplication/Fragment3.java
+++ MyApplication2/app/src/main/java/com/example/myapplication/Fragment3.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,6 +61,25 @@ public class Fragment3 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_3, container, false);
+        View view = inflater.inflate(R.layout.fragment_3, null);
+
+        Button button1 = view.findViewById(R.id.male_button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getActivity(),Tab3Male.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button2 = view.findViewById(R.id.female_button);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getActivity(),Tab3Female.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
