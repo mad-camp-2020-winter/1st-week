152889dee28cd9c137f27c843ed1a85779ade013
DESKTOP-QITT7RJ\q <ghqls1237@kaist.ac.kr>
2021-01-04 18:31:27 +0900

    밸런스 게임 - round16만 됨

--- MyApplication2/app/src/main/java/com/example/myapplication/Tab3Male.java
+++ MyApplication2/app/src/main/java/com/example/myapplication/Tab3Male.java
@@ -19,17 +19,15 @@ public class Tab3Male extends Activity {
 
 
     private Integer[] male = GlobalVariables.male;
-    private Integer[] arr_round8 = male;
-    private Integer[] arr_round4 = new Integer[8];
-    private Integer[] arr_round2 = new Integer[4];
-    private Integer[] arr_round1 = new Integer[2];
+    private Integer[] arr_round16 = GlobalVariables.shuffle(male);
+    private Integer[] arr_round8 = new Integer[8];
+    private Integer[] arr_round4 = new Integer[4];
+    private Integer[] arr_round2 = new Integer[2];
 
+    int countRound16 = 0;
     int countRound8 = 0;
     int countRound4 = 0;
     int countRound2 = 0;
-    int countRound1 = 0;
-
-    Integer[] test = {1,2,3,4,5,6,7,8,9,10};
 
 
     @Override
@@ -37,35 +35,107 @@ public class Tab3Male extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab3_male);
 
-        final Integer[] image_round8 = GlobalVariables.shuffle(GlobalVariables.images);
-
+        final TextView male_round16 = findViewById(R.id.male_round16);
         final TextView male_round8 = findViewById(R.id.male_round8);
         final TextView male_round4 = findViewById(R.id.male_round4);
         final TextView male_round2 = findViewById(R.id.male_round2);
-        final TextView male_round1 = findViewById(R.id.male_round1);
 
+        male_round8.setVisibility(View.INVISIBLE);
         male_round4.setVisibility(View.INVISIBLE);
         male_round2.setVisibility(View.INVISIBLE);
-        male_round1.setVisibility(View.INVISIBLE);
+        male_round16.bringToFront();
+        male_round8.bringToFront();
+        male_round4.bringToFront();
+        male_round2.bringToFront();
+
+
 
         ImageButton button1 = findViewById(R.id.male_first_button);
         ImageButton button2 = findViewById(R.id.male_second_button);
 
-        button1.setImageResource(arr_round8[0]);
-        button2.setImageResource(arr_round8[1]);
+        button1.setImageResource(arr_round16[0]);
+        button2.setImageResource(arr_round16[1]);
+
+        countRound16 = countRound16 + 1;
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                button2.setImageResource(arr_round16[0]);
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                //일단 기존의 데이터를 10ms간 유지
+
+                if (countRound16<=8){
+                    //선택된 이미지를 다음 항목 리스트에 삽입하기
+                    arr_round8[countRound16-1] = arr_round16[(countRound16-1)*2];
+                
+                    if (countRound16 ==8){
+                        male_round8.setVisibility(View.VISIBLE);
+                        male_round16.setVisibility(View.INVISIBLE);
+                        arr_round8 = GlobalVariables.shuffle(arr_round8);
+                        button1.setImageResource(arr_round8[0]);
+                        button2.setImageResource(arr_round8[1]);
+                        countRound8 = countRound8 + 1;
+                    }
+                    else {
+                        //다음에 나올 이미지 띄우기
+                        button1.setImageResource(arr_round16[countRound16 * 2]);
+                        button2.setImageResource(arr_round16[countRound16 * 2 + 1]);
+                    }
+
+                    countRound16 = countRound16 + 1;
+                }
+
+                else if(countRound8<=4){
+                    arr_round4[countRound8-1] = arr_round8[(countRound4-1)*2];
+
+                    if (countRound8 ==8){
+                        male_round4.setVisibility(View.VISIBLE);
+                        male_round8.setVisibility(View.INVISIBLE);
+                        arr_round4 = GlobalVariables.shuffle(arr_round4);
+                        button1.setImageResource(arr_round4[0]);
+                        button2.setImageResource(arr_round4[1]);
+                        countRound4 = countRound4 + 1;
+                    }
+                    else {
+                        //다음에 나올 이미지 띄우기
+                        button1.setImageResource(arr_round8[countRound8 * 2]);
+                        button2.setImageResource(arr_round8[countRound8 * 2 + 1]);
+                    }
+
+                    countRound8 = countRound8 + 1;
+                }
+
+
+
 
 
             }
         });
 
 
+
+
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                button1.setImageResource(arr_round16[0]);
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                //일단 기존의 데이터를 10ms간 유지
+
 
             }
         });
